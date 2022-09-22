7a523f61f966ac90e63ccf1a8dd641d28f9f2ba3
h4r0ldru1z <haroldand.ruiz@gmail.com>
2022-09-22 02:43:35 +0000

    avanzando poco a poco

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-2">
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">
+          <img
+            src="https://seeklogo.com/images/S/Star_Wars-logo-B06952A462-seeklogo.com.png"
+            width="60"
+            height="60"
+          ></img>
+        </span>
+      </Link>
+      <div className="ml-auto">
+        <div className="btn-group">
+          <button
+            type="button"
+            class="btn btn-primary dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Favorites
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
---
+++ src/js/component/personajes.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Personajes =()=>{
+    return(
+        <>
+		<h1>Personajes</h1>
+		<div classNameName="card-group">
+			<div className="row row-cols-1 row-cols-md-3 g-4">
+				<div className="col">
+					<div className="card h-100">
+						<img src="..." className="card-img-top" alt="..."></img>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">.</p>
+						</div>
+						<div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+							<div className="btn-group me-2"role="group" aria-label="First group">
+								<button type="button" className="btn btn-primary">Ver Más!</button>
+							</div>
+							<div className="btn-group me-2" role="group" aria-label="Third group">
+								<button type="button" className="btn btn-outline-warning"><i className="fa fa-heart text-warning" /></button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card h-100">
+						<img src="..." className="card-img-top" alt="..."></img>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">.</p>
+						</div>
+						<div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+							<div className="btn-group me-2"role="group" aria-label="First group">
+								<button type="button" className="btn btn-primary">Ver Más!</button>
+							</div>
+							<div className="btn-group me-2" role="group" aria-label="Third group">
+								<button type="button" className="btn btn-outline-warning"><i className="fa fa-heart text-warning" /></button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card h-100">
+						<img src="..." className="card-img-top" alt="..."></img>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">.</p>
+						</div>
+						<div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+							<div className="btn-group me-2"role="group" aria-label="First group">
+								<button type="button" className="btn btn-primary">Ver Más!</button>
+							</div>
+							<div className="btn-group me-2" role="group" aria-label="Third group">
+								<button type="button" className="btn btn-outline-warning"><i className="fa fa-heart text-warning" /></button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card h-100">
+						<img src="..." className="card-img-top" alt="..."></img>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">.</p>
+						</div>
+						<div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+							<div className="btn-group me-2"role="group" aria-label="First group">
+								<button type="button" className="btn btn-primary">Ver Más!</button>
+							</div>
+							<div className="btn-group me-2" role="group" aria-label="Third group">
+								<button type="button" className="btn btn-outline-warning"><i className="fa fa-heart text-warning" /></button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</>
+    )
+}
